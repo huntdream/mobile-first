b3491c7e05a3718033f60f3631c4e229a98123ad
Yu <maoyu960320@hotmail.com>
2017-08-16 09:57:24 +0800

    add min-width to avatar

--- js/main.js
+++ js/main.js
@@ -1,5 +1,20 @@
-$(document).ready(function(){
-    $('.toggle').on('click',function () {
-        $('.nav-bar-tabs').toggle('showMenu');
-    })
+$(document).ready(function () {
+    const menuWrapper = $('.nav-bar-tabs');
+    const input = $('#message');
+    const text = $('#text');
+    $('.toggle').on('click', function (e) {
+        $(menuWrapper).toggleClass('showMenu');
+    });
+    $(menuWrapper).on('click', function (e) {
+        if (e.target.tagName === 'NAV') {
+            $(menuWrapper).removeClass('showMenu');
+        }
+    });
+
+    $(input).on('focus',function () {
+        $(text).addClass("fly-above");
+    });
+    $(input).on('blur',function () {
+        $(text).removeClass("fly-above");
+    });
 });
