f0bb30337fd84232486ee7081ace7a9cd08fda3d
Yu <maoyu960320@hotmail.com>
2017-08-18 14:41:40 +0800

    add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
 const gulp = require('gulp');
 const imagemin = require('gulp-imagemin');
 
-gulp.task('min',() => {
-    gulp.src('img/B.jpg').pipe(imagemin({optimizationLevel: 9,})).pipe(gulp.dest('dist'));
+gulp.task('min', () => {
+    gulp.src('img/B.jpg').pipe(imagemin({
+        optimizationLevel: 9,
+    })).pipe(gulp.dest('dist'));
+});
+
+gulp.task('ap', () => {
+    var postcss = require('gulp-postcss');
+    var sourcemaps = require('gulp-sourcemaps');
+    var autoprefixer = require('autoprefixer');
+
+    return gulp.src('./css/main.css')
+        .pipe(sourcemaps.init())
+        .pipe(postcss([autoprefixer()]))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./css/autoprefixed'));
 });
